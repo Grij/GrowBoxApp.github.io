7d3d853dce7bb82d6619e120eb1aa5856a717bd0
Grij <52110638+Grij@users.noreply.github.com>
2019-10-16 15:29:11 +0300

    Create date.js

---
+++ js/date.js
@@ -0,0 +1,6 @@
+var todayDate = new Date();
+var currYear = todayDate.getFullYear();
+var currMonth = todayDate.getMonth()+1;
+var currDay = todayDate.getDate();
+document.write("");
+document.write(currDay + "/" + currMonth + "/" + currYear);
